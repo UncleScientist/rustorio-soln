edfb78c418d23e429ff74de34b8b51162e3dc001
Uncle Scientist <unclescientist@gmail.com>
2026-01-28 07:42:57 -0500

    first commit

---
+++ src/bin/tutorial/main.rs
@@ -0,0 +1,26 @@
+#![forbid(unsafe_code)]
+
+use rustorio::{self, Bundle, Tick, gamemodes::Tutorial, resources::Copper};
+
+type GameMode = Tutorial;
+
+type StartingResources = <GameMode as rustorio::GameMode>::StartingResources;
+
+fn main() {
+    rustorio::play::<GameMode>(user_main);
+}
+
+fn user_main(mut tick: Tick, starting_resources: StartingResources) -> (Tick, Bundle<Copper, 4>) {
+    tick.log(true);
+
+    let StartingResources {
+        iron,
+        mut iron_territory,
+        mut copper_territory,
+        guide,
+    } = starting_resources;
+
+    // To start, run the game using `rustorio play tutorial` (or whatever this save is called), and follow the hint.
+    // If you get stuck, try giving the guide other objects you've found, like the `tick` object.
+    guide.hint(iron)
+}
