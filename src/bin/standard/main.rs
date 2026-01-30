973665fba65f403e80bc0242b61c58df1679c0f1
Uncle Scientist <unclescientist@gmail.com>
2026-01-30 07:15:41 -0500

    Got up to electronic circuits

--- src/bin/standard/main.rs
+++ src/bin/standard/main.rs
@@ -4,8 +4,9 @@ use rustorio::{
     self, Bundle, HandRecipe, Resource, Tick,
     buildings::{Assembler, Furnace},
     gamemodes::Standard,
-    recipes::{CopperSmelting, CopperWireRecipe, IronSmelting},
-    resources::{Copper, CopperWire, Point},
+    recipes::{CopperSmelting, CopperWireRecipe, ElectronicCircuitRecipe, IronSmelting},
+    resources::{Copper, CopperOre, CopperWire, Iron, IronOre, Point},
+    territory::Territory,
 };
 
 type GameMode = Standard;
@@ -26,34 +27,22 @@ fn user_main(mut tick: Tick, starting_resources: StartingResources) -> (Tick, Bu
         steel_technology,
     } = starting_resources;
 
-    // make an iron ore furnace
+    // make an iron ore furnace with the 10 iron we're given
     let mut iron_furnace = Furnace::build(&tick, IronSmelting, iron);
 
-    // mine 10 iron ore & smelt it
-    let iron_ore = iron_territory.hand_mine::<10>(&mut tick);
-    iron_furnace.inputs(&tick).0.add(iron_ore);
-    tick.advance_until(|tick| iron_furnace.outputs(tick).0.amount() >= 10, 100);
-
-    let iron = iron_furnace
-        .outputs(&tick)
-        .0
-        .bundle::<10>()
-        .expect("should have gotten 10 iron");
-
-    // use the 10 iron to build a copper ore furnace
+    // mine more iron, and make a copper furnace
+    let iron = handmade_iron(&mut tick, &mut iron_furnace, &mut iron_territory);
     let mut copper_furnace = Furnace::build(&tick, CopperSmelting, iron);
 
     let mut copper_resource = Resource::<Copper>::new_empty();
-    for _ in 0..6 {
-        let copper_ore = copper_territory.hand_mine::<1>(&mut tick);
-        copper_furnace.inputs(&tick).0.add(copper_ore);
-        tick.advance_until(|tick| copper_furnace.outputs(tick).0.amount() >= 1, 100);
-        copper_resource += copper_furnace
-            .outputs(&tick)
-            .0
-            .bundle::<1>()
-            .expect("should have gotten 1 copper");
-    }
+    let copper_ore = copper_territory.hand_mine::<6>(&mut tick);
+    copper_furnace.inputs(&tick).0.add(copper_ore);
+    tick.advance_until(|tick| copper_furnace.outputs(tick).0.amount() >= 6, 100);
+    copper_resource += copper_furnace
+        .outputs(&tick)
+        .0
+        .bundle::<6>()
+        .expect("should have gotten 6 copper");
 
     let mut copper_wire_resource = Resource::<CopperWire>::new_empty();
     for _ in 0..6 {
@@ -66,19 +55,27 @@ fn user_main(mut tick: Tick, starting_resources: StartingResources) -> (Tick, Bu
     }
     let copper_wire_bundle = copper_wire_resource.bundle().expect("expected a bundle");
 
-    let iron_ore = iron_territory.hand_mine::<6>(&mut tick);
-    iron_furnace.inputs(&tick).0.add(iron_ore);
-    tick.advance_until(|tick| iron_furnace.outputs(tick).0.amount() >= 6, 100);
-    let iron_bundle = iron_furnace
-        .outputs(&tick)
-        .0
-        .bundle::<6>()
-        .expect("should have gotten 10 iron");
+    let iron_bundle = handmade_iron(&mut tick, &mut iron_furnace, &mut iron_territory);
 
-    let copper_wire_assembler =
+    let mut copper_wire_assembler =
         Assembler::build(&tick, CopperWireRecipe, copper_wire_bundle, iron_bundle);
 
-    println!("{copper_wire_assembler:?}");
+    let copper = handmade_copper::<6>(&mut tick, &mut copper_furnace, &mut copper_territory);
+    copper_wire_assembler.inputs(&tick).0.add(copper);
+    tick.advance_until(
+        |tick| copper_wire_assembler.outputs(tick).0.amount() >= 12,
+        1_000_000,
+    );
+    let copper_wire = copper_wire_assembler
+        .outputs(&tick)
+        .0
+        .bundle::<12>()
+        .expect("copper wire");
+    let iron = handmade_iron(&mut tick, &mut iron_furnace, &mut iron_territory);
+
+    let mut electronic_circuit_assembler =
+        Assembler::build(&tick, ElectronicCircuitRecipe, copper_wire, iron);
+    dbg!(electronic_circuit_assembler);
 
     /*
     // mine 20 iron and 15 copper for a lab
@@ -117,4 +114,34 @@ fn user_main(mut tick: Tick, starting_resources: StartingResources) -> (Tick, Bu
     todo!("Return the `tick` and the victory resources to win the game!")
 
     // (tick, Bundle<Point, 200>)
+}
+
+fn handmade_iron<const AMOUNT: u32>(
+    tick: &mut Tick,
+    furnace: &mut Furnace<IronSmelting>,
+    territory: &mut Territory<IronOre>,
+) -> Bundle<Iron, AMOUNT> {
+    let iron_ore = territory.hand_mine::<AMOUNT>(tick);
+    furnace.inputs(&tick).0.add(iron_ore);
+    tick.advance_until(|tick| furnace.outputs(tick).0.amount() >= AMOUNT, 1_000_000);
+    furnace
+        .outputs(&tick)
+        .0
+        .bundle::<AMOUNT>()
+        .expect("should have gotten iron")
+}
+
+fn handmade_copper<const AMOUNT: u32>(
+    tick: &mut Tick,
+    furnace: &mut Furnace<CopperSmelting>,
+    territory: &mut Territory<CopperOre>,
+) -> Bundle<Copper, AMOUNT> {
+    let copper_ore = territory.hand_mine::<AMOUNT>(tick);
+    furnace.inputs(&tick).0.add(copper_ore);
+    tick.advance_until(|tick| furnace.outputs(tick).0.amount() >= AMOUNT, 1_000_000);
+    furnace
+        .outputs(&tick)
+        .0
+        .bundle::<AMOUNT>()
+        .expect("should have gotten copper")
 }
